b16a224a4f2580d6940fda271dc23d52d7dbbe2d
Keiran Hines <keiranhines@gmail.com>
2021-12-07 20:36:22 +1100

    Completed Day 7

--- day7.go
+++ day7.go
@@ -17,6 +17,10 @@ func (p PairList) Len() int           { return len(p) }
 func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p PairList) Less(i, j int) bool { return p[j].value < p[i].value } // Less is actually more likely
 
+// FindOptimalPosition finds the optimal position to move all crabs to assuming a cost of 1 fuel cell per move
+// This method assumes, based on the fuel cost the optimal solution will be at a position where one set of crabs do not move
+// input: a comma seperated list of crab positions.
+// Returns the optimal position and the cost.
 func FindOptimalPosition(input string) (int, int) {
 	data := strings.Split(input, ",")
 	mapping := make(map[int]int, len(data))
@@ -45,6 +49,46 @@ func FindOptimalPosition(input string) (int, int) {
 		if bestCount == -1 || sum < bestCount {
 			bestCount = sum
 			bestPos = pair.key
+		}
+	}
+	return bestPos, bestCount
+}
+
+// FindOptimalPositionCostly Finds the optimal position to move all crabs to assuming a cost of n+1 per move where n is the number of
+// positions already moved. E.g. the first move costs 1, the second 3, the third 6,
+// This method assumes the optimal position will be somewhere between the min and max of all starting positions.
+// input: a comma seperated list of crab positions.
+// Returns the optimal position and the cost.
+func FindOptimalPositionCostly(input string) (int, int) {
+	data := strings.Split(input, ",")
+	mapping := make(map[int]int, len(data))
+	minX := -1
+	maxX := -1
+	for _, v := range data {
+		x, _ := strconv.Atoi(v)
+		if minX == -1 || x < minX {
+			minX = x
+		}
+		if maxX < x {
+			maxX = x
+		}
+		mapping[x]++
+	}
+
+	bestPos := -1
+	bestCount := -1
+
+	for i := minX; i <= maxX; i++ {
+		sum := 0
+		for key, value := range mapping {
+			diff := (Max(i, key) - Min(i, key))
+			cost := (float32(diff+1) / float32(2)) * float32(diff)
+			sum += int(cost * float32(value))
+		}
+		// println("Cost of ", pair.key, "is", sum)
+		if bestCount == -1 || sum < bestCount {
+			bestCount = sum
+			bestPos = i
 		}
 	}
 	return bestPos, bestCount
--- day7_test.go
+++ day7_test.go
@@ -29,3 +29,28 @@ func TestDay7Part1(t *testing.T) {
 		t.Fatalf("Expected optimal move count to be %v, got %v", ansCount, moveCount)
 	}
 }
+func TestDay7Example2(t *testing.T) {
+	b := ReadFile("inputs/d7t")
+	input := string(b)
+	optimal, moveCount := FindOptimalPositionCostly(input)
+	if optimal != 5 {
+		t.Fatal("Expected optimal position to be 5, got ", optimal)
+	}
+	if moveCount != 168 {
+		t.Fatal("Expected optimal move count to be 168, got ", moveCount)
+	}
+}
+
+func TestDay7Part2(t *testing.T) {
+	ansPos := 466
+	ansCount := 91638945
+	b := ReadFile("inputs/d7a")
+	input := string(b)
+	optimal, moveCount := FindOptimalPositionCostly(input)
+	if optimal != ansPos {
+		t.Fatalf("Expected optimal position to be %v, got %v", ansPos, optimal)
+	}
+	if moveCount != ansCount {
+		t.Fatalf("Expected optimal move count to be %v, got %v", ansCount, moveCount)
+	}
+}
